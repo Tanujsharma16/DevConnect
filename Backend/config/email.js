ce9240d8cf78dc621dbd7f9e160957e5ea0fcfb4
Tanuj Sharma <tanujpandit16q@gmail.com>
2026-07-23 22:38:28 +0530

    Fix SendGrid sender email

--- Backend/config/email.js
+++ Backend/config/email.js
@@ -3,18 +3,23 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const transporter = {
-    sendMail: async ({ to, subject, text, html }) => {
-
-        const msg = {
-            to,
-            from: "tanujpandit16q@gmail.com",
-            subject,
-            text,
-            html,
-        };
-
-        await sgMail.send(msg);
-    },
+    sendMail: async ({to, subject, text, html}) => {
+        try {
+            await sgMail.send({
+                to,
+                from: "tanujpandit16q@gmail.com",
+                subject,
+                text,
+                html
+            });
+        } catch(err) {
+            console.log(
+              "SENDGRID ERROR:",
+              err.response.body.errors
+            );
+            throw err;
+        }
+    }
 };
 
 module.exports = transporter;
